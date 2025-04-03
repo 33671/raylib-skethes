6d9526494bdc2528367f314915dc6c6b4471714a
Ace Taffy <acetaffy123@outlook.com>
2025-04-03 12:50:42 +0800

    perlin popcorn

--- src/resources/lissajous.fs
+++ src/resources/lissajous.fs
@@ -1,10 +1,11 @@
-// #version 330
 precision highp float;
-uniform float u_time;
+// float map(vec2 p, float r, float t) {
+//     return distance(0.4*vec2(cos(3.0*t), sin(1.0*t)), p) - r;
+// }
 float random (float v) {
     return fract(sin(v * 12.9898) * 43758.5453);
 }
-uniform vec2 u_resolution;
+// uniform vec2 u_resolution;
 // 6t^5 - 15t^4 + 10t^3
 float fade(float t) {
     return t * t * t * (t * (t * 6.0 - 15.0) + 10.0);
@@ -27,18 +28,23 @@ float noise(float x)
     float t = fade(x_fract); // [0,1]
     return lerp(a,b,t);
 }
-
+float map(vec2 p, float r, float t) {
+    return distance(1.0*vec2(noise(1.0*t), noise(t+1.0)), p) - r;
+}
+uniform vec2 u_resolution;
+uniform float u_time;
 void main()
 {
+    const int l = 500;
+   
     vec2 uv = (gl_FragCoord.xy-u_resolution.xy*.5)/u_resolution.y;
-    vec2 perin_x = uv * 10.0;
-    float time = u_time * 0.5;
-    float y = noise(perin_x.x+time);
 
-    float noise_curve = smoothstep(.01,.0,abs(y - uv.y));
-    float stripe =  fract(perin_x.x+time);
-    float aaWidth = 0.01; 
-    float grid_line = smoothstep(0.0, 0.01, stripe) - smoothstep(0.01, 0.06, stripe);
-    float y_axis = (1.0-step(0.002, abs(uv.y)));
-    gl_FragColor  = vec4(vec3(max(max(noise_curve,grid_line),y_axis)),1.0);
+    vec3 col =vec3(0.0);
+    
+    for(int i=0; i<l; i++) { 
+        col += vec3(step(.0,-map(uv, 0.004, u_time-float(i)*0.5)));
+    }
+
+
+    gl_FragColor = vec4(col,1.0);
 }
